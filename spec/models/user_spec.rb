2e7fa8b0ab0cc116ae4cdfc67f685a9cb648ce60
Daniel Paulsen <danielpaulsen@Daniels-MBP.home>
2020-10-22 16:50:59 -0400

    finished user roles and combined with merges to master

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  it { should have_valid(:email).when("user@fake.com")}
+  it { should_not have_valid(:email).when(nil)}
+ 
+  it { should have_valid(:username).when("username")}
+  it { should_not have_valid(:username).when(nil)}
+
+  it { should have_valid(:password).when("password")}
+  it { should_not have_valid(:password).when(nil)}
+
   describe "#admin?" do
     it "is not an admin if the role is not admin" do
       user = FactoryBot.create(:user, role: :member)
