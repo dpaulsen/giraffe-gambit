7f7123da42584f718320ba75dbe1e7f86763192d
Daniel Paulsen <danielpaulsen@Daniels-MBP.home>
2020-10-22 15:10:17 -0400

    minor syntax change

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :giraffe
 
-  validates :rating, presence: true, numericality: { less_than_or_equal_to: 5 , greater_than_or_equal_to: 1 }
+  validates :rating, numericality:{ less_than_or_equal_to: 5 , 
+                                    greater_than_or_equal_to: 1 }
   validates :giraffe, presence: true
 end
