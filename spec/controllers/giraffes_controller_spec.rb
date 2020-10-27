f62c7722faf5f9df5e96f1fb1e62cea58e6631c5
Lucas Encarnacao <lucas.encarnacao5@gmail.com>
2020-10-27 14:09:34 -0400

    WIP fixing testing

--- spec/controllers/giraffes_controller_spec.rb
+++ spec/controllers/giraffes_controller_spec.rb
@@ -1,29 +1,32 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::GiraffesController, type: :controller do
-  let!(:first_giraffe) { Giraffe.create(
-    name: "Hugo",
-    description: "this big guy loves to eat leaves"
-  ) }
-  let!(:second_giraffe) { Giraffe.create(
-    name: "Penelope",
-    description: "this little gal loves to run"
-  ) }
+  let!(:test_user) { FactoryBot.create(:user)}
 
-  let!(:first_review){ Review.create(
-    giraffe: first_giraffe,
-    rating: 5,
-    comment: "Hugo is awesome!!!"
-  )}
-  let!(:second_review){ Review.create(
-    giraffe: first_giraffe,
-    rating: 3
-  )}
+  let!(:first_giraffe) { FactoryBot.create(:giraffe, name: "Hugo", user: test_user) }
+  let!(:second_giraffe) { FactoryBot.create(:giraffe, name: "Penelope", user: test_user) }
 
+  let!(:first_review) { 
+    FactoryBot.create(
+      :review, 
+      comment: "Hugo is awesome!!!", 
+      giraffe: first_giraffe, 
+      owner: test_user
+    ) 
+  }
+
+  let!(:second_review) { 
+    FactoryBot.create(
+      :review, 
+      rating: 3,
+      comment: nil,
+      giraffe: first_giraffe, 
+      owner: test_user
+    ) 
+  }
 
   describe "GET#index" do
     it "should return a list of all the giraffes" do
-
       get :index
       returned_json = JSON.parse(response.body)
 
@@ -31,15 +34,14 @@ RSpec.describe Api::V1::GiraffesController, type: :controller do
       expect(response.content_type).to eq("application/json")
 
       expect(returned_json.length).to eq 2
-  
+
       expect(returned_json[0]["name"]).to eq "Hugo"
-  
       expect(returned_json[1]["name"]).to eq "Penelope"
     end
   end
 
   describe "GET#show" do
-    it "should return an individual giraffe with its name and description" do
+    it "should return an individual giraffe with its name, description and reviews" do
 
       get :show, params: {id: first_giraffe.id}
       returned_json = JSON.parse(response.body)
@@ -51,15 +53,12 @@ RSpec.describe Api::V1::GiraffesController, type: :controller do
       expect(returned_json["name"]).to eq first_giraffe.name
       expect(returned_json["id"]).to eq first_giraffe.id
       expect(returned_json["description"]).to eq first_giraffe.description
-      
+
       expect(returned_json["reviews"].first["rating"]).to eq 5
       expect(returned_json["reviews"].first["comment"]).to eq "Hugo is awesome!!!"
-      expect(returned_json["reviews"].first["giraffe_id"]).to eq first_giraffe.id
-  
+
       expect(returned_json["reviews"].second["rating"]).to eq 3
       expect(returned_json["reviews"].second["comment"]).to eq nil
-      expect(returned_json["reviews"].second["giraffe_id"]).to eq first_giraffe.id
-
     end
   end
 
