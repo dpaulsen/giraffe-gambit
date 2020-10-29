553d7f8ed4128feb89296d3b5a8036b95ed5bec6
Daniel Paulsen <danielpaulsen@Daniels-MBP.home>
2020-10-29 09:43:32 -0400

    edit delete partial front end set up

--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -1,80 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
+import ReviewShowTile from "./ReviewShowTile"
+import ReviewEditTile from "./ReviewEditTile"
 
 const ReviewTile = (props) => {
-  let voteErrorsDiv = null;
-  let commentDiv = null;
+  const [showEditTile, setShowEditTile] = useState(false);
 
-  const onClickHandler = (event) => {
-    let voteChoice = null;
-    if (event.currentTarget.id === "up-vote") {
-      voteChoice = "up";
-    } else if (event.currentTarget.id === "down-vote") {
-      voteChoice = "down";
-    }
 
-    let reviewId = props.review.id;
+  
 
-    props.handleVoteSubmit(reviewId, voteChoice);
-  };
-
-  if (
-    props.voteErrors.message !== "" &&
-    props.voteErrors.reviewId === props.review.id
-  ) {
-    voteErrorsDiv = (
-      <div className="grid-x align-center">
-        <div className="callout alert cell shrink">
-          {props.voteErrors.message}
-        </div>
-      </div>
-    );
+  const onEditClickHandler = (event) =>{
+     // call/load editReview (new form with slightly different styling)
+     setShowEditTile(true);
   }
 
-  if (props.review.comment !== "" && props.review.comment !== null) {
-    commentDiv = (
-      <div className="grid-x grid-margin-x">
-        <h5 className="cell small-2">Comment:</h5>
-        <div className="callout cell small-12">{props.review.comment}</div>
-      </div>
-    );
+  const onDeleteClickHandler = (event) => {
+
+
   }
+
+  const onDiscardClickHandler = (event) => {
+    setShowEditTile(false);
+  }
+
+  const onSaveClickHandler = (event) => {
+
+  }
+
+  const editReview = (payload) =>{
+
+  }
+
+  let displayTile = null
+  //check state
+
+  if ( showEditTile){
+    displayTile =(<ReviewEditTile 
+      review={props.review}
+      editReview= {editReview}
+      onDiscardClickHandler = {onDiscardClickHandler}
+      
+      />)
+      
+    }else {
+      
+      displayTile = (<ReviewShowTile 
+              review={props.review}
+              handleVoteSubmit={props.handleVoteSubmit}
+              voteErrors={props.voteErrors}
+              onEditClickHandler = {onEditClickHandler}
+              onDeleteClickHandler = {onDeleteClickHandler}
+      />);
+
+  }
+
+
 
   return (
-    <div className="grid-container">
-      <div className="callout">
-        <div className="grid-x grid-margin-x align-middle">
-          <h5 className="cell shrink">Rating:</h5>
-          <h6 className="cell auto">{props.review.rating} out of 5</h6>
-        </div>
-
-        {commentDiv}
-
-        <div className="grid-x grid-margin-x align-middle text-center">
-          <button
-            type="button"
-            className="button cell shrink"
-            id="up-vote"
-            onClick={onClickHandler}
-          >
-            Up
-          </button>
-
-          <div className="cell small-1">{props.review.voteCount}</div>
-
-          <button
-            type="button"
-            className="button cell shrink"
-            id="down-vote"
-            onClick={onClickHandler}
-          >
-            Down
-          </button>
-        </div>
-
-        {voteErrorsDiv}
+    <div>
+      <div> 
 
       </div>
+
+     <div>
+      {displayTile}
+    </div> 
     </div>
+   
   );
 };
 
