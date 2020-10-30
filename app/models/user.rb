d16415e5622fb3d7a7b3c2d32afe30a127106057
Daniel Paulsen <danielpaulsen@Daniels-MBP.home>
2020-10-29 20:52:16 -0400

    add role to user model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   has_many :votes
   has_many :voted_reviews, through: :votes , source: :review
 
+  enum role: [:member , :admin ]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :member
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
